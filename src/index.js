45373f535e4a4e7a602b9cc8e160970c28662040
Luis Martinez <mluise2406@gmail.com>
2021-12-06 19:52:07 -0600

    refactoring code

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const app = require("./app")
+const app = require("./modules/app.js")
 
 app.listen(app.get("PORT"), () => {
   console.log(`Server ready on port ${app.get("PORT")}`)
--- src/modules/app.js
+++ src/modules/app.js
@@ -2,13 +2,14 @@ const express = require("express");
 const cors = require("cors")
 const morgan = require("morgan")
 
-const routes = require("./routes/index.routes")
+const routes = require("../routes/index.routes")
 
 const app = express()
 
 app.use(cors())
 app.use(morgan("dev"))
 app.use(express.json())
+app.use(express.static("public"))
 app.use(express.urlencoded({
   extended: false
 }))
