517f80f0dad4fea5eb580d66b07eedc540a8a74f
Luis Martinez <mluise2406@gmail.com>
2021-12-06 19:51:07 -0600

    multer config added

---
+++ src/modules/storage.js
@@ -0,0 +1,13 @@
+const path = require("path")
+const { v4 } = require("uuid")
+const multer = require("multer")
+
+const userStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(__dirname,  'public/img/profile-pics/') );
+    },
+
+    filename: (req, file, cb) => {
+        cb(null, v4() + path.extname(file.extname))
+    }
+})
