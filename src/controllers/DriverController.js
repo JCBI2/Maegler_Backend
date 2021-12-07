0a9f162bfaa54e80275f15799b0591536a8171e0
Luis Martinez <mluise2406@gmail.com>
2021-12-06 19:51:57 -0600

    driver'sregister and driver's login added

--- src/controllers/DriverController.js
+++ src/controllers/DriverController.js
@@ -4,24 +4,56 @@ class DriverController {
     constructor() { }
 
     getDrivers(req, res) {
-        Employee.find().then( (result) => {
-            res.status(200).send({ employees: result})
-        }).catch( err => res.status(400).send({ message: err}))
+        Employee.find().then((result) => {
+            res.status(200).send({ employees: result })
+        }).catch(err => res.status(400).send({ message: err }))
     }
 
-    getDriver(req, res){
-        Employee.findById(req.params.id).then( (result) => {
-            res.status(200).send({employee: result})
-        }).catch( err => res.status(400).send({ message: err}))
+    getDriver(req, res) {
+        Employee.findById(req.params.id).then((result) => {
+            res.status(200).send({ employee: result })
+        }).catch(err => res.status(400).send({ message: err }))
     }
 
-    addNewDriver(req, res) {
+    async registerDriver(req, res) {
         const employee = new Employee(req.body)
-        employee.save().then( (result) => {
-            res.status(201).send({message: "user created"})
-        }).catch( err => res.status(400).send({message: err}))
-
+        employee.password = await employee.encryptPass(employee.password)
+        try{
+            await employee.save()
+            res.status(201).send({ message: "user created" })
+        }catch(err){
+            if(err.code === 11000){
+                res.status(400).send({message: "Email already exists"})
+            }
+        }
     }
+
+    //TODO Mover funcion al controller del admin
+    approveDriver(req, res) {
+        Employee.findByIdAndUpdate(req.params.id, {
+            request_status: "approved"
+        }).then(result => {
+            res.status(200).send({
+                message: "Driver approved"
+            })
+        }).catch(err => res.status(400).send({ message: err }))
+    }
+
+    async loginDriver(req, res) {
+        const employee = await Employee.findOne({email: req.body.email}, { password: 1 })
+        if(!employee){
+            return res.status(404).send({ message: 'User not found'})
+        }
+
+        const userIsValid = await employee.verifyPass(req.body.password, employee.password)
+
+        if(!userIsValid){
+            return res.status(400).send({ message: "Incorrect password"})
+        }
+        
+        res.status(200).send({ message: "User logged"})
+    }
+
 }
 
 module.exports = new DriverController()
--- src/routes/drivers.routes.js
+++ src/routes/drivers.routes.js
@@ -5,9 +5,22 @@ const DriverController = require("../controllers/DriverController.js")
 
 const router = Router();
 
+/** GET  */
+// Obtener lista de repartidores
 router.get("/", DriverController.getDrivers)
+// Obtener información de un repartidor
 router.get("/:id", DriverController.getDriver)
-router.post("/new", DriverController.addNewDriver)
+// Obtener lista de órdenes tomadas por un repartidor
+router.get("/:d/orders", )
 
+/** POST  */
+// Registrar un nuevo rpartidor
+router.post("/register", DriverController.registerDriver)
+// Login del repartidor
+router.post("/login", DriverController.loginDriver)
+
+/** PUT  */
+// Aprobar  repartidor
+router.put("/:id/approve", DriverController.approveDriver)
 
 module.exports = router
