8178eb8153637375842961f4d6ce45ce4ae37f41
jcbi2 <boquinizaguirrej@yahoo.com>
2021-12-11 06:12:01 -0600

    Modelo para obtener categorias

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 const express = require('express')
 const app = require("./modules/app.js")
+const categorysRouter = require('./routes/categorys.routes');
+
+
+app.use('/categorias', categorysRouter);
+
 
 app.listen(app.get("PORT"), () => {
   console.log(`Server ready on port ${app.get("PORT")}`)
